19cdb25d8a1f075f51bbdba66fb8072160ca2b88
Christian J. Balcom <robot.inventor@gmail.com>
2014-12-02 17:48:16 -0500

    Added an asyncio event loop implementation that uses FPGA time.

---
+++ robotpy_ext/misc/asyncio_policy.py
@@ -0,0 +1,26 @@
+"""
+This is a replacement event loop and policy for asyncio that uses FPGA time,
+rather than native python time.
+"""
+from asyncio.events import BaseDefaultEventLoopPolicy
+from asyncio import SelectorEventLoop, set_event_loop_policy
+from wpilib import Timer
+
+
+class FPGATimedEventLoop(SelectorEventLoop):
+    """An asyncio event loop that uses wpilib time rather than python time"""
+    def time(self):
+        return Timer.getFPGATimestamp()
+
+
+class FPGATimedEventLoopPolicy(BaseDefaultEventLoopPolicy):
+    """An asyncio event loop policy that uses FPGATimedEventLoop"""
+    _loop_factory = FPGATimedEventLoop
+
+
+def patch_asyncio_policy():
+    """
+    Sets an instance of FPGATimedEventLoopPolicy as the default asyncio event
+    loop policy
+    """
+    set_event_loop_policy(FPGATimedEventLoopPolicy())
