42b2a58b58bc00cc698608b454ae0085200ba79d
Dustin Spicuzza <dustin@virtualroadside.com>
2016-05-18 22:50:56 -0400

    Add magicbot to coverage

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -3,6 +3,6 @@
 set -e
 cd `dirname $0`
 
-PYTHONPATH=.. python3 -m coverage run --source robotpy_ext -m pytest $@
+PYTHONPATH=.. python3 -m coverage run --source robotpy_ext,magicbot -m pytest $@
 python -m coverage report -m
 
