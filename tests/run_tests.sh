1527069a39969ba422b9bcecad6b85209ecb75ec
Dustin Spicuzza <dustin@virtualroadside.com>
2017-01-06 23:02:31 -0500

    Don't run coverage by default

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,8 +1,12 @@
-#!/bin/bash
+#!/bin/bash -e
 
-set -e
-cd `dirname $0`
+cd $(dirname $0)
 
-PYTHONPATH=.. python3 -m coverage run --source robotpy_ext,magicbot -m pytest $@
-python -m coverage report -m
+export PYTHONPATH=".."
 
+if [ "$RUNCOVERAGE" == "1" ]; then
+    python3 -m coverage run --source robotpy_ext,magicbot -m pytest "$@"
+    python3 -m coverage report -m
+else
+    python3 -m pytest "$@"
+fi
