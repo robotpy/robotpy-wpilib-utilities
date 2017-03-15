robotpy/robotpy-wpilib-utilities

commit bd5dc2043355c9f127c4e357b36d0284053d4c6b

Fix deploy to test at same speed as --test

robotpy_ext/misc/precise_delay.py
@@ -38,7 +38,7 @@ class PreciseDelay:
             # to run more efficiently -- otherwise full tests are dog slow
             try:
                 import pyfrc.config
-                if pyfrc.config.mode == 'test':
+                if pyfrc.config.mode == 'test' or pyfrc.config.mode == 'deploy':
                     self.wait = self._wait_unit_tests
             except:
                 pass

commit da66772d1622134a364ddaef5edab5920798cd1c

Actually fix deploy timing to be the same as tests
Fixes: bd5dc2043355c9f127c4e357b36d0284053d4c6b

robotpy_ext/misc/precise_delay.py
@@ -38,7 +38,7 @@ class PreciseDelay:
             # to run more efficiently -- otherwise full tests are dog slow
             try:
                 import pyfrc.config
-                if pyfrc.config.mode == 'test' or pyfrc.config.mode == 'deploy':
+                if pyfrc.config.mode in ('test', 'upload'):
                     self.wait = self._wait_unit_tests
             except:
                 pass