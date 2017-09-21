b466e0c41629575e0661aff1ba37c7056a732e0a
Dustin Spicuzza <dustin@virtualroadside.com>
2017-09-21 01:00:08 -0400

    Add default_state to the magicbot exports

--- magicbot/__init__.py
+++ magicbot/__init__.py
@@ -3,4 +3,4 @@ from .magicrobot import MagicRobot
 from .magic_tunable import tunable
 from .magic_reset import will_reset_to
 
-from .state_machine import AutonomousStateMachine, StateMachine, state, timed_state
+from .state_machine import AutonomousStateMachine, StateMachine, default_state, state, timed_state
