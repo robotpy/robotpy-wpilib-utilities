483329fe127cb2d05067ab7f36835deab5cc1884
Dustin Spicuzza <dustin@virtualroadside.com>
2026-03-13 04:43:58 +0000

    feat: implement keep-alive MagicBot command lifecycle

--- magicbot/commands.py
+++ magicbot/commands.py
@@ -4,8 +4,40 @@ import commands2
 
 
 class CommandRunner:
+    def __init__(self) -> None:
+        self._requested: set[commands2.Command] = set()
+        self._active: set[commands2.Command] = set()
+
     def run(self, command: commands2.Command) -> None:
-        raise NotImplementedError
+        self._requested.add(command)
 
     def execute(self) -> None:
-        raise NotImplementedError
+        stale = [
+            command
+            for command in self._active
+            if command not in self._requested
+            and command.getInterruptionBehavior()
+            != commands2.Command.InterruptionBehavior.kCancelIncoming
+        ]
+        for command in stale:
+            command.end(True)
+            self._active.remove(command)
+
+        new_commands = [
+            command for command in self._requested if command not in self._active
+        ]
+        for command in new_commands:
+            command.initialize()
+            self._active.add(command)
+
+        finished: list[commands2.Command] = []
+        for command in list(self._active):
+            command.execute()
+            if command.isFinished():
+                command.end(False)
+                finished.append(command)
+
+        for command in finished:
+            self._active.remove(command)
+
+        self._requested.clear()
